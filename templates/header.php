690c725f3ff3dd27269cbb5df432f84415c26c85
DOULORES <lucas.carvalho30111@gmail.com>
2025-04-19 10:13:47 -0300

    Cria os templates e adiciona div para msgs de sucesso ou erro no login

---
+++ templates/header.php
@@ -0,0 +1,55 @@
+<?php 
+
+    require_once('globals.php');
+    require_once('db.php');
+
+    $flashMessage = array();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiaDourado</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="<?= $BASE_URL?>img/logo.ico" type="image/x-icon">
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.css" integrity="sha512-drnvWxqfgcU6sLzAJttJv7LKdjWn0nxWCSbEAtxJ/YYaZMyoNLovG7lPqZRdhgL1gAUfa+V7tbin8y+2llC1cw==" crossorigin="anonymous" />
+    <!-- Fonte Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL?>css/styles.css">
+</head>
+<body>
+    <header>
+        <nav id="main-navbar" class="navbar navbar-expand-lg">
+            <a href="<?= $BASE_URL?>" class="navbar-brand">
+                <img src="<?= $BASE_URL?>img/logo-guia-dourado.png" alt="GuiaDourado" id="logo">
+                <span id="guia-dourado-title">GuiaDourado</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <form action="<?= $BASE_URL ?>search.php" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+                <button class="btn my-2 my-sm-0" type="submit">
+                <i class="fas fa-search"></i>
+                </button>
+            </form>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="<?= $BASE_URL?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <?php if(!empty($flashMessage['msg'])): ?>
+        <div class="msg-container">
+            <p class="msg <?= $flashMessage['type']?>"><?= $flashMessage['msg'] ?></p>
+        </div>
+    <?php endif;?>
+    
