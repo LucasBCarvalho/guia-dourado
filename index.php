072b9a5ad874b4c1b5b42549838be35b6dc5adb1
DOULORES <lucas.carvalho30111@gmail.com>
2025-04-19 09:59:11 -0300

    Começa estilização da

--- index.php
+++ index.php
@@ -11,19 +11,65 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GuiaDourado</title>
     <!-- Favicon -->
-    <link rel="shortcut icon" href="assets/img/logo.ico" type="image/x-icon">
-    <link rel="icon" href="assets/img/logo.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="<?= $BASE_URL?>img/logo.ico" type="image/x-icon">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.css" integrity="sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.css" integrity="sha512-drnvWxqfgcU6sLzAJttJv7LKdjWn0nxWCSbEAtxJ/YYaZMyoNLovG7lPqZRdhgL1gAUfa+V7tbin8y+2llC1cw==" crossorigin="anonymous" />
     <!-- Fonte Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS -->
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL?>css/styles.css">
 </head>
 <body>
-    <h1>GuiaDourado</h1>
-    
-    <i class="far fa-plus-square"></i>
+    <header>
+        <nav id="main-navbar" class="navbar navbar-expand-lg">
+            <a href="<?= $BASE_URL?>" class="navbar-brand">
+                <img src="<?= $BASE_URL?>img/logo-guia-dourado.png" alt="GuiaDourado" id="logo">
+                <span id="guia-dourado-title">GuiaDourado</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <form action="<?= $BASE_URL ?>search.php" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+                <button class="btn my-2 my-sm-0" type="submit">
+                <i class="fas fa-search"></i>
+                </button>
+            </form>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="<?= $BASE_URL?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <div id="main-container" class="container-fluid">
+        <h1>Corpo do Site</h1>
+    </div>
+    <footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li>
+                    <a href="" class="fab fa-facebook-square"></a>
+                </li>
+                <li>
+                    <a href="" class="fab fa-instagram"></a>
+                </li>
+                <li>
+                    <a href="" class="fab fa-youtube"></a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li><a href="#">Adicionar Clinica</a></li>
+                <li><a href="#">Adicionar Crítica</a></li>
+                <li><a href="#">Adicionar Entrar/Registrar</a></li>
+            </ul>
+        </div>
+        <p>&copy; GuiaDourado</p>
+    </footer>
     <!-- Bootstrap JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.js" integrity="sha512-lsA4IzLaXH0A+uH6JQTuz6DbhqxmVygrWv1CpC/s5vGyMqlnP0y+RYt65vKxbaVq+H6OzbbRtxzf+Zbj20alGw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
