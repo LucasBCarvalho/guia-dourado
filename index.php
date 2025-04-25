a5d802bf7851b910b9f30d911be10568a3a0aa65
DOULORES <lucas.carvalho30111@gmail.com>
2025-04-25 20:12:35 -0300

    Começa criação da Home

--- index.php
+++ index.php
@@ -3,7 +3,34 @@
 ?>
 
 <div id="main-container" class="container-fluid">
-    <h1>Corpo do Site</h1>
+    <h2 class="section-title">Novas Clínicas</h2>
+    <p class="section-description">Veja as críticas das últimas clínicas adicionadas</p>
+    <div class="clinic-container">
+        <div class="card clinic-card">
+            <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/clinics/clinics_cover.jpg')"></div>
+                <div class="card-body">
+                    <p class="card-rating">
+                        <i class="fas fa-star"></i>
+                        <span class="rating">9</span>
+                    </p>
+                    <h5 class="card-title">
+                        <a href="#">Título da Clínica</a>
+                    </h5>
+                    <a href="#" class="btn btn-primary rate-btn">Avaliar</a>
+                    <a href="#" class="btn btn-primary card-btn">Conhecer</a>
+                </div>
+            </div>
+    </div>
+    <h2 class="section-title">Clínicas de Categoria 1</h2>
+    <p class="section-description">Veja as melhores clínicas para idosos categoria 1</p>
+    <div class="clinic-container">
+        
+    </div>
+    <h2 class="section-title">Clínicas de Categoria 1 e 2</h2>
+    <p class="section-description">Veja as melhores clínicas para idosos categoria 1 e 2</p>
+    <div class="clinic-container">
+        
+    </div>
 </div>
     
 <?php 
